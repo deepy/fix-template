7383a5ca97ba04bca0b90a8de3a4583ffe59afe5
Alex Nordlund <deep.alexander@gmail.com>
2018-05-25 18:20:02 +0200

    Changing Stylify to jinja2 filters.

--- gen.py
+++ gen.py
@@ -1,27 +1,33 @@
-from jinja2 import Environment, FileSystemLoader, select_autoescape
-
-env = Environment(
-    loader=FileSystemLoader('templates'),
-    autoescape=select_autoescape(['html'])
-)
+from jinja2 import Environment, FileSystemLoader, select_autoescape, evalcontextfilter, Markup
 
 
 class Stylify:
     def __init__(self, conf):
         self.conf = conf
 
-    def linkify(self, obj):
-        return "../{}.html".format(self.conf.get_http_path(obj))
+    @evalcontextfilter
+    def linkify(self, eval_ctx, obj):
+        result = "../{}.html".format(self.conf.get_http_path(obj))
+        if eval_ctx.autoescape:
+            return Markup(result)
+        return result
 
-    def boldify(self, obj, text):
+    @evalcontextfilter
+    def boldify(self, eval_ctx, obj, text):
         if obj.pretty_type() == 'Component':
-            return "<b>{}</b>".format(text)
+            result = "<b>{}</b>".format(text)
+            if eval_ctx.autoescape:
+                return Markup(result)
+            return result
         else:
             return text
 
 
 def fiximate(conf, subdir, messages, lookup, repo, copyright=None):
-    template = env.select_template([subdir+'.html', 'messages.html'])
+    env = Environment(
+        loader=FileSystemLoader('templates'),
+        autoescape=select_autoescape(['html'])
+    )
 
     import os
     path = conf.get_paths(subdir)
@@ -29,8 +35,13 @@ def fiximate(conf, subdir, messages, lookup, repo, copyright=None):
 
     stylify = Stylify(conf)
 
+    env.filters['boldify'] = stylify.boldify
+    env.filters['linkify'] = stylify.linkify
+
+    template = env.select_template([subdir + '.html', 'messages.html'])
+
     for message in messages:
         filename = '{}.html'.format(conf.get_filename(message))
         with open(os.path.join(path, filename), 'w', encoding='utf-8') as outfile:
-            result = template.render(message=message, lookup=lookup, stylify=stylify, copyright=copyright, repository=repo)
+            result = template.render(message=message, lookup=lookup, copyright=copyright, repository=repo)
             outfile.write(result)
