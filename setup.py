8d8518fceb6f45572d18889fe20710f239552f02
Alex Nordlund <deep.alexander@gmail.com>
2018-06-13 19:59:17 +0200

    Releasing 0.0.2, improved documentation and somewhat feature-complete.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="FIXation",
-    version="0.0.1",
+    version="0.0.2",
     author="Alex Nordlund",
     author_email="deep.alexander@gmail.com",
     description="FIX repository tool",
